9bd8117e54a522c1ff8339dcfe638d9cc14819c2
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2025-12-13 21:27:07 -0800

    Refactor `print` methods

--- src/2025/01.c
+++ src/2025/01.c
@@ -1,32 +1,16 @@
+#include "print.h"
 #include "register.h"
-
-#include <stdio.h>
-#include <stdint.h>
-
-static inline void print_lld(int64_t x)          { printf("%lld\n", x); }
-static inline void print_llu(uint64_t x)         { printf("%llu\n", x); }
-static inline void print_s(const char* x)        { printf("%s\n", x); }
-
-#define answer(x) _Generic((x), \
-    long long:          print_lld, \
-    unsigned long long: print_llu, \
-    char*:              print_s, \
-    const char*:        print_s  \
-)(x)
-
-#define submit_a(x) do { printf("Part A: "); answer(x); } while(0)
-#define submit_b(x) do { printf("Part B: "); answer(x); } while(0)
 
 static void part_a(const char* input)
 {
     int64_t ans = 42;
-    submit_a(ans);
+    print_a(ans);
 }
 
 static void part_b(const char* input)
 {
     const char* ans = "Hello, AoC 2025!";
-    submit_b(ans);
+    print_b(ans);
 }
 
 REGISTER_SOLUTION(
---
+++ src/print.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdint.h>
+
+// clang-format off
+static inline void print_lld(int64_t x)   { printf("%lld\n", x); }
+static inline void print_llu(uint64_t x)  { printf("%llu\n", x); }
+static inline void print_s(const char* x) { printf("%s\n", x); }
+
+#define answer(x) _Generic((x), \
+    long long:          print_lld, \
+    unsigned long long: print_llu, \
+    char*:              print_s, \
+    const char*:        print_s  \
+)(x)
+
+#define print_a(x) do { printf("Part A: "); answer(x); } while(0)
+#define print_b(x) do { printf("Part B: "); answer(x); } while(0)
+// clang-format on
