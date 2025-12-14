cf409c1e6b4823fb2eb07c88337d614ff471a9d9
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2025-12-13 21:29:06 -0800

    Formatting

--- src/register.h
+++ src/register.h
@@ -5,13 +5,13 @@
 #define CONCAT(a, b) CONCAT_(a, b)
 
 #ifdef __APPLE__
-#define REGISTER_SOLUTION(...) \
-    static const AoCSolution CONCAT(__aoc_sol_, __LINE__) = { __VA_ARGS__ }; \
-    static const AoCSolution* CONCAT(__aoc_ptr_, __LINE__) \
-    __attribute__((used, section("__DATA,aoc_solutions"))) = &CONCAT(__aoc_sol_, __LINE__);
+#define REGISTER_SOLUTION(...)                                             \
+    static const AoCSolution CONCAT(__aoc_sol_, __LINE__) = {__VA_ARGS__}; \
+    static const AoCSolution* CONCAT(__aoc_ptr_, __LINE__)                 \
+        __attribute__((used, section("__DATA,aoc_solutions"))) = &CONCAT(__aoc_sol_, __LINE__);
 #else
-#define REGISTER_SOLUTION(...) \
-    static const AoCSolution CONCAT(__aoc_sol_, __LINE__) = { __VA_ARGS__ }; \
-    static const AoCSolution* CONCAT(__aoc_ptr_, __LINE__) \
-    __attribute__((used, section("aoc_solutions"))) = &CONCAT(__aoc_sol_, __LINE__);
+#define REGISTER_SOLUTION(...)                                             \
+    static const AoCSolution CONCAT(__aoc_sol_, __LINE__) = {__VA_ARGS__}; \
+    static const AoCSolution* CONCAT(__aoc_ptr_, __LINE__)                 \
+        __attribute__((used, section("aoc_solutions"))) = &CONCAT(__aoc_sol_, __LINE__);
 #endif
