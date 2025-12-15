a3a88306b2ff66e1f7d5ecf028de3dbc952c9f50
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2025-12-14 22:40:28 -0800

    wip 2025 02

---
+++ src/2025/02.c
@@ -0,0 +1,33 @@
+#include "print.h"
+#include "register.h"
+#include "stdlib.h"
+
+static void solve(const char* input)
+{
+    int part_a = 0;
+    int part_b = 0;
+
+    const char* p = input;
+    while (*p) {
+        uint64_t range_start, range_end;
+        while (*p >= '0' && *p <= '9') {
+            range_start = range_start * 10 + (uint64_t)(*p - '0');
+            ++p;
+        }
+
+        ++p; // Move past the `-`
+
+        while (*p >= '0' && *p <= '9') {
+            range_end = range_end * 10 + (uint64_t)(*p - '0');
+            ++p;
+        }
+
+        ++p; // Move past the comma
+
+    }
+
+    print_a(part_a);
+    print_b(part_b);
+}
+
+REGISTER_SOLUTION(.year = 2025, .day = 2, .solve = solve)
